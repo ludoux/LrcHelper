70e1879d893b5b049914f8a7ba0b55ce07f1a713
Chang Lu <chinaluchang@live.com>
2017-02-04 11:16:09 +0800

    v1.0.1 Release
    Use TPL in order to speed up.
    Update README.md
    Fix bugs.

--- LrcHelper/LrcDownloader.cs
+++ LrcHelper/LrcDownloader.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
 using Ludoux.LrcHelper.NeteaseMusic;
+using System.Diagnostics;
 
 namespace LrcHelper
 {
@@ -15,16 +18,19 @@ namespace LrcHelper
 
         private void GETbutton_Click(object sender, EventArgs e)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             int ID = Convert.ToInt32(IDtextBox.Text);
             if (MusicradioButton.Checked)
             {
                 Music m = new Music(ID);
                 int status;
                 string Log = DownloadLrc(ID, 100, ".\\" + m.GetFileName() + ".lrc",out status);
+                sw.Stop();
                 if (Log == "")
-                    MessageBox.Show("Done Status:"+status);
+                    MessageBox.Show("Done Status:" + status + "\r\nUsed Time:" + sw.Elapsed.TotalSeconds +"sec");
                 else
-                    MessageBox.Show(Log + " Status:" + status);
+                    MessageBox.Show(Log + " Status:" + status + "\r\nUsed Time:" + sw.Elapsed.TotalSeconds + "sec");
             }
             else if(PlaylistradioButton.Checked)
             {
@@ -32,24 +38,28 @@ namespace LrcHelper
                 Playlist pl = new Playlist(ID);
                 List<int> iDList = pl.SongIDInPlaylist;
                 string folderName = pl.GetFolderName();
+                for (int i = 0; i < iDList.Count; i++)
+                    Log.Add("");
                 if (!System.IO.Directory.Exists(".\\" + folderName))
                     System.IO.Directory.CreateDirectory(".\\"+folderName);
                 
-                for (int i = 0; i < iDList.Count; i++)
+                Parallel.For ( 0, iDList.Count, i =>
                 {
                     Music m = new Music(iDList[i]);
                     int status;
-                    string ErrorLog = DownloadLrc(iDList[i], 100, ".\\" + folderName +@"\"+ m.GetFileName() + ".lrc",out status);
-                    if(System.IO.File.Exists(".\\" + folderName + @"\" + m.GetFileName() + ".lrc"))
-                        Log.Add(string.Format("{0,-7}|{1,-12}|{2,-50}|{3,-6}|√" + ErrorLog, i, iDList[i], m.Name, status));
+                    string ErrorLog = DownloadLrc(iDList[i], 100, ".\\" + folderName + @"\" + m.GetFileName() + ".lrc", out status);
+                    if (System.IO.File.Exists(".\\" + folderName + @"\" + m.GetFileName() + ".lrc"))
+                        Log[i] = string.Format("{0,-7}|{1,-12}|{2,-50}|{3,-6}|√" + ErrorLog, i+1, iDList[i], m.Name, status);
                     else
-                        Log.Add(string.Format("{0,-7}|{1,-12}|{2,-50}|{3,-6}|×" + ErrorLog, i, iDList[i], m.Name, status));
-                }
+                        Log[i] = string.Format("{0,-7}|{1,-12}|{2,-50}|{3,-6}|×" + ErrorLog, i+1, iDList[i], m.Name, status);
+                });
+                sw.Stop();
                 StringBuilder OutLog = new StringBuilder();
-                OutLog.Append(DateTime.Now.ToString() + " PlaylistID:" + ID + " PlaylistName:" + pl.Name + " TotalCount:" + iDList.Count + "\r\n0为无人上传歌词,1为有词,2为纯音乐,-1错误,-2未命中");
+                OutLog.Append("PlaylistID:" + ID + "\r\nPlaylistName:" + pl.Name + "\r\nTotalCount:" + iDList.Count + "\r\n0为无人上传歌词,1为有词,2为纯音乐,-1错误,-2未命中\r\n");
                 OutLog.Append(string.Format("\r\n{0,-7}|{1,-12}|{2,-50}|{3,-6}|ErrorInfo", "SongNum", "SongID", "SongName", "LrcSts"));
                 for (int i = 0; i < Log.Count; i++)
                     OutLog.Append("\r\n" + Log[i]);
+                OutLog.Append("\r\n\r\n" + DateTime.Now.ToString() + "  Used Time:" + sw.Elapsed.TotalSeconds + "sec\r\n[re:Made by LrcHelper @https://github.com/ludoux/lrchelper]\r\n[ve:" + FileVersionInfo.GetVersionInfo(Application.ExecutablePath).FileVersion + "]\r\nEnjoy music with lyrics now!(*^_^*)");
                 System.IO.File.WriteAllText(".\\" + folderName + @"\Log.txt", OutLog.ToString(), Encoding.UTF8);
             }
         }
