ab0654dfbe9c03388cb2e7a9e4cf053cdc8c5318
Lu Chang <chinaluchang@live.com>
2022-01-04 15:39:17 +0800

    2.3.1 Released.
    fix: 上传错了版本……

--- LrcHelper/NeteaseMusic.cs
+++ LrcHelper/NeteaseMusic.cs
@@ -18,7 +18,7 @@ namespace ludoux.LrcHelper.NeteaseMusic
     }
     class HttpRequest
     {
-        public string GetContent(string sURL, string cookie ="",string UserAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36 Edg/96.0.1054.62")
+        public string GetContent(string sURL, string cookie ="",string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62")
         {
             
             string sContent = ""; //Content
